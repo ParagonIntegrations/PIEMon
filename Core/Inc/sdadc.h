990081ab30dea3743bbcde8a0d2fb9e268fd1b8f
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-09-20 14:25:51 +0200

    changed cubemx to generate seperate files for peripherals

---
+++ Core/Inc/sdadc.h
@@ -0,0 +1,54 @@
+/**
+  ******************************************************************************
+  * @file    sdadc.h
+  * @brief   This file contains all the function prototypes for
+  *          the sdadc.c file
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SDADC_H__
+#define __SDADC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern SDADC_HandleTypeDef hsdadc1;
+extern SDADC_HandleTypeDef hsdadc2;
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+void MX_SDADC1_Init(void);
+void MX_SDADC2_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SDADC_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Core/Src/sdadc.c
@@ -0,0 +1,318 @@
+/**
+  ******************************************************************************
+  * @file    sdadc.c
+  * @brief   This file provides code for the configuration
+  *          of the SDADC instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "sdadc.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+SDADC_HandleTypeDef hsdadc1;
+SDADC_HandleTypeDef hsdadc2;
+DMA_HandleTypeDef hdma_sdadc1;
+DMA_HandleTypeDef hdma_sdadc2;
+
+/* SDADC1 init function */
+void MX_SDADC1_Init(void)
+{
+
+  /* USER CODE BEGIN SDADC1_Init 0 */
+
+  /* USER CODE END SDADC1_Init 0 */
+
+  SDADC_ConfParamTypeDef ConfParamStruct = {0};
+
+  /* USER CODE BEGIN SDADC1_Init 1 */
+
+  /* USER CODE END SDADC1_Init 1 */
+  /** Configure the SDADC low power mode, fast conversion mode,
+  slow clock mode and SDADC1 reference voltage
+  */
+  hsdadc1.Instance = SDADC1;
+  hsdadc1.Init.IdleLowPowerMode = SDADC_LOWPOWER_NONE;
+  hsdadc1.Init.FastConversionMode = SDADC_FAST_CONV_DISABLE;
+  hsdadc1.Init.SlowClockMode = SDADC_SLOW_CLOCK_DISABLE;
+  hsdadc1.Init.ReferenceVoltage = SDADC_VREF_EXT;
+  hsdadc1.InjectedTrigger = SDADC_SOFTWARE_TRIGGER;
+  if (HAL_SDADC_Init(&hsdadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the Injected Mode
+  */
+  if (HAL_SDADC_SelectInjectedDelay(&hsdadc1, SDADC_INJECTED_DELAY_NONE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_SDADC_SelectInjectedTrigger(&hsdadc1, SDADC_SOFTWARE_TRIGGER) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_SDADC_InjectedConfigChannel(&hsdadc1, SDADC_CHANNEL_0|SDADC_CHANNEL_1
+                              |SDADC_CHANNEL_2, SDADC_CONTINUOUS_CONV_ON) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Set parameters for SDADC configuration 0 Register
+  */
+  ConfParamStruct.InputMode = SDADC_INPUT_MODE_SE_ZERO_REFERENCE;
+  ConfParamStruct.Gain = SDADC_GAIN_1;
+  ConfParamStruct.CommonMode = SDADC_COMMON_MODE_VSSA;
+  ConfParamStruct.Offset = 0;
+  if (HAL_SDADC_PrepareChannelConfig(&hsdadc1, SDADC_CONF_INDEX_0, &ConfParamStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the Injected Channel
+  */
+  if (HAL_SDADC_AssociateChannelConfig(&hsdadc1, SDADC_CHANNEL_0, SDADC_CONF_INDEX_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the Injected Channel
+  */
+  if (HAL_SDADC_AssociateChannelConfig(&hsdadc1, SDADC_CHANNEL_1, SDADC_CONF_INDEX_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the Injected Channel
+  */
+  if (HAL_SDADC_AssociateChannelConfig(&hsdadc1, SDADC_CHANNEL_2, SDADC_CONF_INDEX_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SDADC1_Init 2 */
+
+  /* USER CODE END SDADC1_Init 2 */
+
+}
+/* SDADC2 init function */
+void MX_SDADC2_Init(void)
+{
+
+  /* USER CODE BEGIN SDADC2_Init 0 */
+
+  /* USER CODE END SDADC2_Init 0 */
+
+  SDADC_ConfParamTypeDef ConfParamStruct = {0};
+
+  /* USER CODE BEGIN SDADC2_Init 1 */
+
+  /* USER CODE END SDADC2_Init 1 */
+  /** Configure the SDADC low power mode, fast conversion mode,
+  slow clock mode and SDADC1 reference voltage
+  */
+  hsdadc2.Instance = SDADC2;
+  hsdadc2.Init.IdleLowPowerMode = SDADC_LOWPOWER_NONE;
+  hsdadc2.Init.FastConversionMode = SDADC_FAST_CONV_DISABLE;
+  hsdadc2.Init.SlowClockMode = SDADC_SLOW_CLOCK_DISABLE;
+  hsdadc2.Init.ReferenceVoltage = SDADC_VREF_EXT;
+  hsdadc2.InjectedTrigger = SDADC_SYNCHRONOUS_TRIGGER;
+  if (HAL_SDADC_Init(&hsdadc2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the Injected Mode
+  */
+  if (HAL_SDADC_SelectInjectedDelay(&hsdadc2, SDADC_INJECTED_DELAY_NONE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_SDADC_SelectInjectedTrigger(&hsdadc2, SDADC_SYNCHRONOUS_TRIGGER) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_SDADC_InjectedConfigChannel(&hsdadc2, SDADC_CHANNEL_0|SDADC_CHANNEL_1
+                              |SDADC_CHANNEL_2, SDADC_CONTINUOUS_CONV_ON) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Set parameters for SDADC configuration 0 Register
+  */
+  ConfParamStruct.InputMode = SDADC_INPUT_MODE_SE_ZERO_REFERENCE;
+  ConfParamStruct.Gain = SDADC_GAIN_1;
+  ConfParamStruct.CommonMode = SDADC_COMMON_MODE_VSSA;
+  ConfParamStruct.Offset = 0;
+  if (HAL_SDADC_PrepareChannelConfig(&hsdadc2, SDADC_CONF_INDEX_0, &ConfParamStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the Injected Channel
+  */
+  if (HAL_SDADC_AssociateChannelConfig(&hsdadc2, SDADC_CHANNEL_0, SDADC_CONF_INDEX_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the Injected Channel
+  */
+  if (HAL_SDADC_AssociateChannelConfig(&hsdadc2, SDADC_CHANNEL_1, SDADC_CONF_INDEX_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the Injected Channel
+  */
+  if (HAL_SDADC_AssociateChannelConfig(&hsdadc2, SDADC_CHANNEL_2, SDADC_CONF_INDEX_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SDADC2_Init 2 */
+
+  /* USER CODE END SDADC2_Init 2 */
+
+}
+
+void HAL_SDADC_MspInit(SDADC_HandleTypeDef* sdadcHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(sdadcHandle->Instance==SDADC1)
+  {
+  /* USER CODE BEGIN SDADC1_MspInit 0 */
+
+  /* USER CODE END SDADC1_MspInit 0 */
+    /* SDADC1 clock enable */
+    __HAL_RCC_SDADC1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    /**SDADC1 GPIO Configuration
+    PE10     ------> SDADC1_AIN2P
+    PE11     ------> SDADC1_AIN1P
+    PE12     ------> SDADC1_AIN0P
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    /* SDADC1 DMA Init */
+    /* SDADC1 Init */
+    hdma_sdadc1.Instance = DMA2_Channel3;
+    hdma_sdadc1.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_sdadc1.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_sdadc1.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_sdadc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    hdma_sdadc1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+    hdma_sdadc1.Init.Mode = DMA_CIRCULAR;
+    hdma_sdadc1.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_sdadc1) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(sdadcHandle,hdma,hdma_sdadc1);
+
+  /* USER CODE BEGIN SDADC1_MspInit 1 */
+
+  /* USER CODE END SDADC1_MspInit 1 */
+  }
+  else if(sdadcHandle->Instance==SDADC2)
+  {
+  /* USER CODE BEGIN SDADC2_MspInit 0 */
+
+  /* USER CODE END SDADC2_MspInit 0 */
+    /* SDADC2 clock enable */
+    __HAL_RCC_SDADC2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    /**SDADC2 GPIO Configuration
+    PE13     ------> SDADC2_AIN2P
+    PE14     ------> SDADC2_AIN1P
+    PE15     ------> SDADC2_AIN0P
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    /* SDADC2 DMA Init */
+    /* SDADC2 Init */
+    hdma_sdadc2.Instance = DMA2_Channel4;
+    hdma_sdadc2.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_sdadc2.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_sdadc2.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_sdadc2.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    hdma_sdadc2.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+    hdma_sdadc2.Init.Mode = DMA_CIRCULAR;
+    hdma_sdadc2.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_sdadc2) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    __HAL_LINKDMA(sdadcHandle,hdma,hdma_sdadc2);
+
+  /* USER CODE BEGIN SDADC2_MspInit 1 */
+
+  /* USER CODE END SDADC2_MspInit 1 */
+  }
+}
+
+void HAL_SDADC_MspDeInit(SDADC_HandleTypeDef* sdadcHandle)
+{
+
+  if(sdadcHandle->Instance==SDADC1)
+  {
+  /* USER CODE BEGIN SDADC1_MspDeInit 0 */
+
+  /* USER CODE END SDADC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SDADC1_CLK_DISABLE();
+
+    /**SDADC1 GPIO Configuration
+    PE10     ------> SDADC1_AIN2P
+    PE11     ------> SDADC1_AIN1P
+    PE12     ------> SDADC1_AIN0P
+    */
+    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12);
+
+    /* SDADC1 DMA DeInit */
+    HAL_DMA_DeInit(sdadcHandle->hdma);
+  /* USER CODE BEGIN SDADC1_MspDeInit 1 */
+
+  /* USER CODE END SDADC1_MspDeInit 1 */
+  }
+  else if(sdadcHandle->Instance==SDADC2)
+  {
+  /* USER CODE BEGIN SDADC2_MspDeInit 0 */
+
+  /* USER CODE END SDADC2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SDADC2_CLK_DISABLE();
+
+    /**SDADC2 GPIO Configuration
+    PE13     ------> SDADC2_AIN2P
+    PE14     ------> SDADC2_AIN1P
+    PE15     ------> SDADC2_AIN0P
+    */
+    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15);
+
+    /* SDADC2 DMA DeInit */
+    HAL_DMA_DeInit(sdadcHandle->hdma);
+  /* USER CODE BEGIN SDADC2_MspDeInit 1 */
+
+  /* USER CODE END SDADC2_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
