b6cd18ae94387ca943e5408e0cf8fbb58dde91b7
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-10-13 16:27:38 +0200

    data is saved on the sd card

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -269,6 +269,60 @@ void calculateVIPF(){
     memset(&channels_loop, 0, sizeof(channels_cycle));
 }
 
+void storeresults(){
+    // some variables for FatFs
+    FATFS FatFs; 	//Fatfs handle
+    FIL fil; 		//File handle
+    volatile FRESULT fres; //Result after operations
+
+    // Open the file system
+    f_mount(&FatFs, "", 1); //1=mount now
+
+    for (int n = 0; n < NUMCHANNELS; n++){
+        char filename[9];
+        sprintf(filename, "Channel%i", n);
+
+        //Now let's try and write a file
+        fres = f_open(&fil, filename, FA_WRITE | FA_OPEN_ALWAYS);
+        if(fres == FR_OK) {
+            myprintf("I was able to open 'write.txt' for writing\r\n");
+        } else {
+            myprintf("f_open error (%i)\r\n", fres);
+        }
+        // Go to the end of the file so we can append
+        f_lseek(&fil, f_size(&fil));
+
+        char writebuff[500];
+        sprintf(writebuff, "%.2f,",channels_vipf[n].Vrms);
+        sprintf(writebuff + strlen(writebuff), "%.2f,",channels_vipf[n].Irms);
+        sprintf(writebuff + strlen(writebuff), "%.2f,",channels_vipf[n].realpower_import);
+        sprintf(writebuff + strlen(writebuff), "%.2f,",channels_vipf[n].realpower_export);
+        sprintf(writebuff + strlen(writebuff), "%.2f,",channels_vipf[n].apparentpower);
+        sprintf(writebuff + strlen(writebuff), "%.2f,",channels_vipf[n].power_factor);
+        sprintf(writebuff + strlen(writebuff), "%.2f,",channels_vipf[n].time);
+        sprintf(writebuff + strlen(writebuff), "%.2f,",channels_vipf[n].frequency);
+        sprintf(writebuff + strlen(writebuff), "%.6f,",channels_vipf[n].units_import);
+        sprintf(writebuff + strlen(writebuff), "%.6f,",channels_vipf[n].units_export);
+        sprintf(writebuff + strlen(writebuff), "%.6f,",channels_vipf[n].import_counter);
+        sprintf(writebuff + strlen(writebuff), "%.6f\r\n",channels_vipf[n].export_counter);
+
+        UINT bytesWrote;
+        fres = f_write(&fil, writebuff, strlen(writebuff), &bytesWrote);
+        if(fres == FR_OK) {
+            myprintf("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
+        } else {
+            myprintf("f_write error (%i)\r\n");
+        }
+
+        // Close the file when done
+        f_close(&fil);
+    }
+
+    // We're done, so de-mount the drive
+    f_mount(NULL, "", 0);
+
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -310,112 +364,22 @@ int main(void)
   /* USER CODE BEGIN 2 */
     start_SDADCs();
     HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_4);
-//    char *serialsenddata = "Hello";
 
-    myprintf("\r\n~ SD card demo by kiwih ~\r\n\r\n");
-
-    HAL_Delay(1000); //a short delay is important to let the SD card settle
-
-    //some variables for FatFs
-    FATFS FatFs; 	//Fatfs handle
-    FIL fil; 		//File handle
-    FRESULT fres; //Result after operations
-
-    //Open the file system
-    fres = f_mount(&FatFs, "", 1); //1=mount now
-    if (fres != FR_OK) {
-        myprintf("f_mount error (%i)\r\n", fres);
-        while(1);
-    }
-
-    //Let's get some statistics from the SD card
-    DWORD free_clusters, free_sectors, total_sectors;
-
-    FATFS* getFreeFs;
-
-    fres = f_getfree("", &free_clusters, &getFreeFs);
-    if (fres != FR_OK) {
-        myprintf("f_getfree error (%i)\r\n", fres);
-        while(1);
-    }
-
-    //Formula comes from ChaN's documentation
-    total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
-    free_sectors = free_clusters * getFreeFs->csize;
-
-    myprintf("SD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
-
-    //Now let's try to open file "test.txt"
-    fres = f_open(&fil, "test.txt", FA_READ);
-    if (fres != FR_OK) {
-        myprintf("f_open error (%i)\r\n");
-        while(1);
-    }
-    myprintf("I was able to open 'test.txt' for reading!\r\n");
-
-    //Read 30 bytes from "test.txt" on the SD card
-    BYTE readBuf[30];
-
-    //We can either use f_read OR f_gets to get data out of files
-    //f_gets is a wrapper on f_read that does some string formatting for us
-    TCHAR* rres = f_gets((TCHAR*)readBuf, 30, &fil);
-    if(rres != 0) {
-        myprintf("Read string from 'test.txt' contents: %s\r\n", readBuf);
-    } else {
-        myprintf("f_gets error (%i)\r\n", fres);
-    }
-
-    //Be a tidy kiwi - don't forget to close your file!
-    f_close(&fil);
-
-    //Now let's try and write a file "write.txt"
-    fres = f_open(&fil, "write.txt", FA_WRITE | FA_OPEN_ALWAYS);
-    if(fres == FR_OK) {
-        myprintf("I was able to open 'write.txt' for writing\r\n");
-    } else {
-        myprintf("f_open error (%i)\r\n", fres);
-    }
-    uint32_t  filesize = f_size(&fil);
-    f_lseek(&fil, filesize);
-
-
-    //Copy in a string
-    strncpy((char*)readBuf, "a new file is made!\r\n", 21);
-    UINT bytesWrote;
-    fres = f_write(&fil, readBuf, 21, &bytesWrote);
-    if(fres == FR_OK) {
-        myprintf("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
-    } else {
-        myprintf("f_write error (%i)\r\n");
-    }
-
-    //Be a tidy kiwi - don't forget to close your file!
-    f_close(&fil);
-
-    //We're done, so de-mount the drive
-    f_mount(NULL, "", 0);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-      for( int i = 1000; i < 1500; i += 500 ){
-          HAL_Delay(i);
-          HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
-//          HAL_UART_Transmit(&huart1,(uint8_t*)serialsenddata, strlen(serialsenddata),1000);
-
+      // Calculate averages and transmit data to base station, ensure this can run constantly or the process will fail.
+      if (newcycle) {
+          addcycle();
       }
-      // Calculate averages and transmit data to base station
-//      if (newcycle) {
-//          addcycle();
-//      }
-//      if (cyclecount >= LOOPCYCLES){
-//          calculateVIPF();
-//          HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
-//          switchrelays();
-//          sendresults();
-//      }
+      if (cyclecount >= LOOPCYCLES){
+          calculateVIPF();
+          storeresults();
+          HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+      }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
